e09d4b12d3b975b304ba32f92b1e1dc18c548a8e
jzrheyang <48862057+jzrheyang@users.noreply.github.com>
2020-05-12 22:38:00 +0800

    Update AppEureka.java

--- src/main/java/com/jzr/AppEureka.java
+++ src/main/java/com/jzr/AppEureka.java
@@ -1,5 +1,5 @@
 package com.jzr;
-//添加注释 jzr
+//添加注释 jzr4444
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
